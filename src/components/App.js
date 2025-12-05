34cb6103b596b2938173de7baf2c73d4658b613b
Akash <dalbhanjanakash1@gmail.com>
2025-12-05 21:39:50 +0530

    attempt 4

--- src/components/App.js
+++ src/components/App.js
@@ -2,14 +2,19 @@
 import React from "react";
 import './../styles/App.css';
 import Tooltip from "./Tooltip";
-const App = () => {
-  return (
-    
-    <div>
-        {/* Do not remove the main div */}
-        <Tooltip text = "This is a tooltip" text2="This is another tooltip"/>
-    </div>
-  )
-}
 
-export default App
+export default function App() {
+  return (
+    <div style={{ padding: "50px" }}>
+      <Tooltip text="This is a tooltip">
+        <h1>Hover over me</h1>
+      </Tooltip>
+
+      <hr />
+
+      <Tooltip text="Another tooltip for this text">
+        <p>Hover over me to see another tooltip</p>
+      </Tooltip>
+    </div>
+  );
+}
--- src/components/Tooltip.js
+++ src/components/Tooltip.js
@@ -1,70 +1,18 @@
 import React, { useState } from "react";
 
-export default function Tooltip({ text, text2 }) {
-  const [show, setShow] = useState(false);
-  const [show2, setShow2] = useState(false);
+
+export default function Tooltip({ text, children }) {
+  const [visible, setVisible] = useState(false);
 
   return (
-    <>
-      <div style={{ position: "relative", display: "inline-block" }}>
-        <h2
-          className="tooltip"
-          onMouseEnter={() => setShow(true)}
-          onMouseLeave={() => setShow(false)}
-        >
-          Hover Over me
-        </h2>
+    <div 
+      className="tooltip"
+      onMouseEnter={() => setVisible(true)}
+      onMouseLeave={() => setVisible(false)}
+    >
+      {children}
 
-        {show && (
-          <div
-            style={{
-              position: "absolute",
-              top: "40px",
-              left: 0,
-              background: "red",
-              color: "white",
-              padding: "8px",
-              borderRadius: "6px",
-              fontSize: "14px",
-              whiteSpace: "nowrap",
-              zIndex: 100,
-            }}
-          >
-            {text}
-          </div>
-        )}
-      </div>
-
-      <hr />
-
-      <div style={{ position: "relative", display: "inline-block" }}>
-        <p
-          className="tooltip"
-          onMouseEnter={() => setShow2(true)}
-          onMouseLeave={() => setShow2(false)}
-        >
-          Hover over me to see another tooltip
-        </p>
-
-        {show2 && (
-          <div
-            style={{
-              position: "absolute",
-              top: "40px",
-              left: 0,
-              background: "red",
-              color: "white",
-              padding: "8px",
-              borderRadius: "6px",
-              fontSize: "14px",
-              whiteSpace: "nowrap",
-              zIndex: 100,
-            }}
-          >
-            {text2}
-          </div>
-        )}
-      </div>
-    </>
+      {visible && <span className="tooltiptext">{text}</span>}
+    </div>
   );
 }
